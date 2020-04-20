70d951a8369055a0ec4ae739419e97c98a133649
kejriwalayushi28 <ayushikejriwal.28@gmail.com>
2020-04-21 01:01:01 +0530

    ADD basic website

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export class Footer extends Component {
+  render() {
+    return [
+      <div className="container-fluid dkf_1 fixed-bottom">
+        Copyright 2018 lifestyle.com . All Rights Reserved.
+      </div>
+    ];
+  }
+}
